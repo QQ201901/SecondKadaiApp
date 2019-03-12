2b7ab1ac4964939604829311670c97a696d8fc7a
QQ <oldcapital1990@gmail.com>
2019-03-12 18:12:47 +0900

    Initial Commit

---
+++ Lessen4_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lessen4_6
+//
+//  Created by KUNI K on 2019/03/12.
+//  Copyright © 2019 QQ201901. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
